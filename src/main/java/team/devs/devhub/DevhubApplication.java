58f4b7568b096864b713a9cbc5e21e263523dd14
xogus3492 <wkdxogus3492@gmail.com>
2024-06-27 10:40:18 +0900

    initial commit

---
+++ src/main/java/team/devs/devhub/DevhubApplication.java
@@ -0,0 +1,13 @@
+package team.devs.devhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class DevhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/devs/devhub/DevhubApplicationTests.java
@@ -0,0 +1,13 @@
+package team.devs.devhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
