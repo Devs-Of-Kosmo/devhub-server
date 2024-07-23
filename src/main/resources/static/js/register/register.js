6b456bee634fa9fdb2106478f29c0fd75f52ae25
Dongkyu <wlsntus55@gmail.com>
2024-07-23 11:28:27 +0900

    회원가입 이메일 인증버튼 & 로그인 화면 구글 로그인 버튼 추가

--- src/main/resources/static/js/register/register.js
+++ src/main/resources/static/js/register/register.js
@@ -1,9 +1,21 @@
-
 $(document).ready(function() {
     // 전체 동의 체크박스 클릭 시 개별 체크박스 모두 선택/해제
     $('#flexCheckAll').on('change', function() {
         var isChecked = $(this).is(':checked');
         $('input[type="checkbox"]').prop('checked', isChecked);
+    });
+
+    // 이메일 인증 버튼 클릭 시
+    $('#emailVerifyBtn').on('click', function() {
+        var email = $('#email').val();
+        // 이메일 유효성 검사
+        var emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+        if (!emailPattern.test(email)) {
+            alert('유효한 이메일 주소를 입력하세요.');
+        } else {
+            alert('유효한 이메일 주소입니다.');
+        }
     });
 
     $('#registerForm').on('submit', function(event) {
@@ -18,6 +30,15 @@ $(document).ready(function() {
             return; // 폼 제출 중단
         }
 
+        // 비밀번호 확인 검증
+        var password = $('#password').val();
+        var confirmPassword = $('#confirm_password').val();
+
+        if (password !== confirmPassword) {
+            alert('비밀번호가 일치하지 않습니다.');
+            return; // 폼 제출 중단
+        }
+
         // 필수 이용약관 동의 확인
         if (!$('#termsCheck1').is(':checked') || !$('#termsCheck2').is(':checked')) {
             alert('서비스 이용약관과 개인정보 처리방침에 동의해야 합니다.');
@@ -27,7 +48,7 @@ $(document).ready(function() {
         var formData = {
             name: $('#name').val(),
             email: $('#email').val(),
-            password: $('#password').val()
+            password: password
         };
 
         $.ajax({
@@ -40,10 +61,9 @@ $(document).ready(function() {
                 location.href = '/';
             },
             error: function(error) {
-                alert('Registration failed!');
-                // 회원가입 실패 시 처리할 내용 추가
+                console.error('Error:', error);
+                alert('비밀번호는 8글자이상에 특수문자를 하나 포함해야합니다.');
             }
         });
-
     });
 });
