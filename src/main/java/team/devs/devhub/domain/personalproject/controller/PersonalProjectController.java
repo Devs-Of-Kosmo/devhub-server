92cf0a841a954510fb5df9c65b3d7d6aebecdb09
xogus3492 <wkdxogus3492@gmail.com>
2024-07-28 20:22:19 +0900

    docs: swagger 개인 프로젝트 관련 명칭 수정

--- src/main/java/team/devs/devhub/domain/personalproject/controller/PersonalProjectController.java
+++ src/main/java/team/devs/devhub/domain/personalproject/controller/PersonalProjectController.java
@@ -25,7 +25,7 @@ public class PersonalProjectController {
     private final PersonalProjectService personalProjectService;
 
     @PostMapping("/create")
-    @Operation(summary = "개인 프로젝트 생성 API", description = "header에 accessToken과 body에 projectName과 description을 담아 요청한다")
+    @Operation(summary = "개인 레포지토리 생성 API", description = "header에 accessToken과 body에 projectName과 description을 담아 요청한다")
     public ResponseEntity<PersonalProjectRepoCreateResponse> createPersonalProjectRepo(
             @RequestBody @Valid PersonalProjectRepoCreateRequest request,
             @AuthenticationPrincipal CustomUserDetails customUserDetails
@@ -35,7 +35,7 @@ public class PersonalProjectController {
     }
 
     @GetMapping("/read")
-    @Operation(summary = "개인 프로젝트 목록 조회 API", description = "header에 accessToken을 담아 요청하면 레포지토리 목록을 리스트 형태로 반환한다")
+    @Operation(summary = "개인 레포지토리 목록 조회 API", description = "header에 accessToken을 담아 요청하면 레포지토리 목록을 리스트 형태로 반환한다")
     public ResponseEntity<List<PersonalProjectRepoReadResponse>> readPersonalProjectRepo(
             @AuthenticationPrincipal CustomUserDetails customUserDetails
     ) {
