129be43023e36a0aae6ac0d28506fda336a9de17
xogus3492 <wkdxogus3492@gmail.com>
2024-08-12 18:05:00 +0900

    feat : 팀 프로젝트 팀 생성 기능 구현
    swagger 문서화

---
+++ src/main/java/team/devs/devhub/domain/team/domain/team/repository/TeamRepository.java
@@ -0,0 +1,7 @@
+package team.devs.devhub.domain.team.domain.team.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team.devs.devhub.domain.team.domain.team.Team;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+}
