de495ca930e7c81c1153160b8723c457f5cca992
Dongkyu <wlsntus55@gmail.com>
2024-08-15 15:48:13 +0900

    Feature : (인증) 정리

--- src/main/java/team/devs/devhub/global/config/SecurityConfig.java
+++ src/main/java/team/devs/devhub/global/config/SecurityConfig.java
@@ -36,8 +36,7 @@ public class SecurityConfig {
             "/favicon.ico", "/error",
       
             "/api/*/public/**", "/ws/message",
-            "/api/team/**",
-      
+
             "/", "/register", "/login","/mypage",
             "loading","/personal_project","/project_list",
             "/team_loading","/team_project","/team_project_list/**",
