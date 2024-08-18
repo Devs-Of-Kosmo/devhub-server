198cdb6bc3d2bd61336c4a146a2e055a976c84aa
Dongkyu <wlsntus55@gmail.com>
2024-08-18 16:59:14 +0900

    Feature : (index) 풋터디자인 변경과 이메일 alert 디자인

--- src/main/resources/static/js/message.js
+++ src/main/resources/static/js/message.js
@@ -40,7 +40,6 @@ $(document).ready(function() {
             success: function(response) {
                 userEmail = response.email;
                 updateMessageLinksVisibility();
-                updateMessageImg();
             },
             error: function(xhr, status, error) {
                 console.error('사용자 정보를 가져오는데 실패했습니다:', status, error);
@@ -50,7 +49,6 @@ $(document).ready(function() {
     } else {
         updateMessageLinksVisibility();
     }
-
 
     $('#inviteForm').on('submit', function(event) {
         event.preventDefault();
@@ -71,25 +69,53 @@ $(document).ready(function() {
                     content: messageContent
                 })
             })
-            .then(response => {
-                if (!response.ok) {
-                    return response.json().then(errorData => {
-                        alert("받는 사람의 이메일이 존재하지 않습니다.");
-                    });
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (data) {
-                    alert('메시지가 성공적으로 전송되었습니다!');
-                    $('#inviteModal').modal('hide');
-                } else {
-                    alert('메시지 전송에 실패했습니다.');
-                }
-            })
-            .catch(error => console.error('Error:', error));
-        }else{alert("300자 이내로 작성해주세요." + messageStrCount +"/300");}
+                .then(response => {
+                    if (!response.ok) {
+                        return response.json().then(errorData => {
+                            Swal.fire('실패', '받는 사람의 이메일이 존재하지 않습니다.', 'error');
+                        });
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    if (data) {
+                        Swal.fire('성공', '메시지가 성공적으로 전송되었습니다!', 'success');
+                        $('#inviteModal').modal('hide');
+                    } else {
+                        Swal.fire('실패', '메시지 전송에 실패했습니다.', 'error');
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    Swal.fire('실패', '메시지 전송 중 오류가 발생했습니다.', 'error');
+                });
+        } else {
+            Swal.fire('실패', `300자 이내로 작성해주세요. (${messageStrCount}/300)`, 'error');
+        }
     });
+
+    function deleteMessage(messageId, messageBox, url, messageType) {
+        fetch(`/api/messages/delete?id=${messageId}&box=${messageBox}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            }
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Network response was not ok');
+                }
+            })
+            .then(responseData => {
+                Swal.fire('성공', '쪽지가 삭제 되었습니다.', 'success');
+                loadMessages(url, messageType);
+            })
+            .catch(error => {
+                Swal.fire('실패', '삭제 요청이 실패했습니다: ' + error, 'error');
+            });
+    }
 
     function loadMessages(url, messageType) {
         fetch(url, {
@@ -98,130 +124,90 @@ $(document).ready(function() {
                 'Authorization': 'Bearer ' + accessToken
             }
         })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
 
-            if (!Array.isArray(data)) {
-                throw new Error("Invalid response format");
-            }
-
-            const messageList = $('#messageList');
-            messageList.empty();
-
-            data.forEach(message => {
-                let listItem;
-                if(messageType === 'received' && message.readCondition == true) {
-                    listItem = $('<li></li>').addClass('list-group-item').css({'padding': '8px 20px', 'background-color': '#ededed', 'box-shadow' : 'inset 0 0 5px rgba(0, 0, 0, 0.5)'});
-                } else {
-                    listItem = $('<li></li>').addClass('list-group-item').css('padding', '8px 20px');
+                if (!Array.isArray(data)) {
+                    throw new Error("Invalid response format");
                 }
 
-                let messageTitle = message.content.substring(0, 20);
-                let messageHtml = message.content.replace(/\n/g, "<br>");
+                const messageList = $('#messageList');
+                messageList.empty();
 
-                listItem.append(
-                    $('<div></div>').text(`보낸 사람: ${message.senderEmail}`).css('font-size', 'small'),
-                    $('<div></div>').text(`팀 소개: ${messageTitle}...`).css('font-size', 'small'),
-                    $('<div></div>').css({'display': 'flex', 'justify-content': 'space-between', 'align-items': 'center'}).append(
-                        $('<span></span>').text(`보낸 시간: ${new Date(message.createdDate).toLocaleString()}`).css('font-size', 'small'),
-                        $('<button></button>').addClass('btn btn-danger btn-sm').text('삭제').css({'font-size': 'small', 'border': '2px solid #ff6161', 'background-color': '#ff6161','padding' : '0px 3px', 'margin-right' : '5px'}).on('click', function(event) {
-                            event.stopPropagation();
-                            const parentLi = $(this).closest('li');
-                            const messageId = parentLi.find('.message-id').val();
-                            const messageBox = parentLi.find('.message-box').val();
+                data.forEach(message => {
+                    let listItem;
+                    if(messageType === 'received' && message.readCondition == true) {
+                        listItem = $('<li></li>').addClass('list-group-item').css({'padding': '8px 20px', 'background-color': '#ededed', 'box-shadow' : 'inset 0 0 5px rgba(0, 0, 0, 0.5)'});
+                    } else {
+                        listItem = $('<li></li>').addClass('list-group-item').css('padding', '8px 20px');
+                    }
 
-                            fetch(`/api/messages/delete?id=${messageId}&box=${messageBox}`, {
-                                method: 'GET',
-                                headers: {
-                                    'Authorization': 'Bearer ' + accessToken
-                                }
+                    let messageTitle = message.content.substring(0, 20);
+                    let messageHtml = message.content.replace(/\n/g, "<br>");
+
+                    listItem.append(
+                        $('<div></div>').text(`보낸 사람: ${message.senderEmail}`).css('font-size', 'small'),
+                        $('<div></div>').text(`팀 소개: ${messageTitle}...`).css('font-size', 'small'),
+                        $('<div></div>').css({'display': 'flex', 'justify-content': 'space-between', 'align-items': 'center'}).append(
+                            $('<span></span>').text(`보낸 시간: ${new Date(message.createdDate).toLocaleString()}`).css('font-size', 'small'),
+                            $('<button></button>').addClass('btn btn-danger btn-sm').text('삭제').css({'font-size': 'small', 'border': '2px solid #ff6161', 'background-color': '#ff6161','padding' : '0px 3px', 'margin-right' : '5px'}).on('click', function(event) {
+                                event.stopPropagation();
+                                const parentLi = $(this).closest('li');
+                                const messageId = parentLi.find('.message-id').val();
+                                const messageBox = parentLi.find('.message-box').val();
+                                deleteMessage(messageId, messageBox, url, messageType);
                             })
-                            .then(response => {
-                                if (response.ok) {
-                                    return response.json();
-                                } else {
-                                    throw new Error('Network response was not ok');
-                                }
-                            })
-                            .then(responseData => {
-                                alert('쪽지가 삭제 되었습니다.');
-                                loadMessages(url, messageType);
-                            })
-                            .catch(error => {
-                                alert('삭제 요청이 실패했습니다: ' + error);
-                            });
+                        ),
+                        $('<input type="hidden">').addClass('message-id').val(message.id),
+                        $('<input type="hidden">').addClass('message-box').val(messageType)
+                    ).on('click', function() {
+                        const messageId = $(this).find('.message-id').val();
+                        const messageBox = $(this).find('.message-box').val();
+                        fetch(`/api/messages/${messageBox}/read?id=${messageId}`, {
+                            method: 'GET',
+                            headers: {
+                                'Authorization': 'Bearer ' + accessToken
+                            }
                         })
-                    ),
-                    $('<input type="hidden">').addClass('message-id').val(message.id),
-                    $('<input type="hidden">').addClass('message-box').val(messageType)
-                ).on('click', function() {
-                    const messageId = $(this).find('.message-id').val();
-                    const messageBox = $(this).find('.message-box').val();
-                    fetch(`/api/messages/${messageBox}/read?id=${messageId}`, {
-                        method: 'GET',
-                        headers: {
-                            'Authorization': 'Bearer ' + accessToken
-                        }
-                    })
-                    .then(response => response.json())
-                    .then(data => {
-                        $('#inviteModal').modal('hide');
+                            .then(response => response.json())
+                            .then(data => {
+                                $('#inviteModal').modal('hide');
 
-                        const messageItem = $('<li></li>').addClass('list-group-item').append(
-                            $('<div></div>').css({'display': 'flex', 'align-items': 'center'}).append(
-                                $('<button></button>').addClass('btn btn-secondary btn-sm').text('<').on('click', function() {
-                                    messageList.empty();
-                                    loadMessages(url, messageType);
-                                }),
-                                $('<span></span>').text(`보낸 사람: ${message.senderEmail}`).css({'margin-left': '10px'})
-                            ),
-                            $('<hr>').css('margin-top','3px'),
-                            $('<span></span>').html(`${messageHtml}`).css({'display': 'block', 'text-align' : 'center'}),
-                            $('<hr>').css('margin-bottom','5px'),
-                            $('<div></div>').css({'display': 'flex', 'justify-content': 'space-between', 'align-items': 'center'}).append(
-                                $('<span></span>').text(`보낸 날짜 : ${new Date(message.createdDate).toLocaleString()}`).css('margin-left','5px'),
-                                $('<button></button>').addClass('btn btn-danger btn-sm').text('삭제').css({'border': '2px solid #ff6161', 'background-color': '#ff6161','padding' : '3px 6px', 'margin-right' : '5px'}).on('click', function(event) {
-                                    event.stopPropagation();
+                                const messageItem = $('<li></li>').addClass('list-group-item').append(
+                                    $('<div></div>').css({'display': 'flex', 'align-items': 'center'}).append(
+                                        $('<button></button>').addClass('btn btn-secondary btn-sm').text('<').on('click', function() {
+                                            messageList.empty();
+                                            loadMessages(url, messageType);
+                                        }),
+                                        $('<span></span>').text(`보낸 사람: ${message.senderEmail}`).css({'margin-left': '10px'})
+                                    ),
+                                    $('<hr>').css('margin-top','3px'),
+                                    $('<span></span>').html(`${messageHtml}`).css({'display': 'block', 'text-align' : 'center'}),
+                                    $('<hr>').css('margin-bottom','5px'),
+                                    $('<div></div>').css({'display': 'flex', 'justify-content': 'space-between', 'align-items': 'center'}).append(
+                                        $('<span></span>').text(`보낸 날짜 : ${new Date(message.createdDate).toLocaleString()}`).css('margin-left','5px'),
+                                        $('<button></button>').addClass('btn btn-danger btn-sm').text('삭제').css({'border': '2px solid #ff6161', 'background-color': '#ff6161','padding' : '3px 6px', 'margin-right' : '5px'}).on('click', function(event) {
+                                            event.stopPropagation();
+                                            deleteMessage(messageId, messageBox, url, messageType);
+                                        })
+                                    ),
+                                    $('<hr>').css('margin-top','5px')
+                                );
 
-                                    fetch(`/api/messages/delete?id=${messageId}&box=${messageBox}`, {
-                                        method: 'GET',
-                                        headers: {
-                                            'Authorization': 'Bearer ' + accessToken
-                                        }
-                                    })
-                                    .then(response => {
-                                        if (response.ok) {
-                                            return response.json();
-                                        } else {
-                                            throw new Error('Network response was not ok');
-                                        }
-                                    })
-                                    .then(responseData => {
-                                        alert('쪽지가 삭제 되었습니다.');
-                                        loadMessages(url, messageType);
-                                    })
-                                    .catch(error => {
-                                        alert('삭제 요청이 실패했습니다: ' + error);
-                                    });
-                                })
-                            ),
-                            $('<hr>').css('margin-top','5px')
-                        );
+                                messageList.empty();
+                                messageList.append(messageItem);
+                            })
+                            .catch(error => console.error('Error:', error));
+                    });
 
-                        messageList.empty();
-                        messageList.append(messageItem);
-                    })
-                    .catch(error => console.error('Error:', error));
+                    messageList.append(listItem);
                 });
-
-                messageList.append(listItem);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                Swal.fire('실패', '메시지를 불러오는데 실패했습니다. 다시 시도해주세요.', 'error');
             });
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            alert('메시지를 불러오는데 실패했습니다. 다시 시도해주세요.');
-        });
     }
 
     $('#receivedTab').on('click', function() {
