b66fc48c3f3a630b4e4010217ed080592905b004
xogus3492 <wkdxogus3492@gmail.com>
2024-09-10 15:40:05 +0900

    feat : 팀 프로젝트 팀원 퇴출 기능 구현

--- src/main/java/team/devs/devhub/global/error/exception/ErrorCode.java
+++ src/main/java/team/devs/devhub/global/error/exception/ErrorCode.java
@@ -62,6 +62,7 @@ public enum ErrorCode {
     INVITE_EMAIL_MISMATCH(500, "T020", "팀에 초대된 유저가 아닙니다"),
     INVITE_MAIL_EXPIRED(500, "T021", "초대 유효기간이 지났습니다"),
     MANAGER_ACTION_NOT_ALLOWED(500, "T022", "MANAGER 권환은 불가능합니다"),
+    CANNOT_KICK_MANAGER(500, "T022", "MANAGER 권한의 팀원은 퇴출이 불가능합니다"),
 
     // Board
     BOARD_NOT_FOUND(500,"B001", "게시글을 찾을 수 없습니다"),
